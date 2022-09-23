adfe738cc6bd2949f37943c310e9ffda7210b17a
Vidhant007 <thapavidhant@gmail.com>
2022-09-23 10:24:26 +0530

    content added to file by vidhant

--- storyTeller.py
+++ storyTeller.py
@@ -1,1 +1,9 @@
 #this file contains code for story-teller project
+# Importing random module
+import random
+
+Sentence_starter = ['About 100 years ago', ' In the 20 BC', 'Once upon a time']
+character = [' there lived a king.',' there was a man named Jack.',
+             ' there lived a farmer.']
+time = [' One day', ' One full-moon night']
+
